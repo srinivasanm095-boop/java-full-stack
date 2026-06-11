dfe3549f7d01663274984850bc83107deec41f7e
srinivasanm095-boop <srinivasanm095@gmail.com>
2026-06-11 14:56:43 +0530

    HTML-CSS

--- HTML-CSS/script.js
+++ HTML-CSS/script.js
@@ -1,67 +1,26 @@
-/*let name="padmanadhan";
-var age=18;
-const height="1.72";
-console.log(age);
-console.log(age+5);
-console.log(age*2);
-console.log(age-5);
-console.log(age/2);
-console.log(age%4);
-age+=3;
-console.log(age);
-age-=2;
-console.log(age);
-age*=2;
-console.log(age);
-age/=2;
-console.log(age);
-age%=3;
-console.log(age);
-age++;
-console.log(age);
-age--;
-console.log(age);
-let age1=20;
-console.log(age<age1);
-console.log(age>age1);
-console.log(age<=age1);
-console.log(age==age1);
-let amount=2300;
-let notes500=parseInt(amount/500);
-amount=amount%500;
-let notes200=parseInt(amount/200);
-amount=amount%2000;
-let notes100=parseInt(amount/100);
-amount=amount%100;
-console.log("500 notes:"+notes500);
-console.log("200 notes:"+notes200);
-console.log("100 notes:"+notes100);
-let user_name="padmanadhan";
-let password="12345";
-let name="hero";
-let pin="00000"
-let name_1="zero";
-let password_1="09876";
-if(name=="hero"){
-    console.log(user_name);
-    if(pin=="12345"){
-        console.log("loged in successfully");
+/*function num(n)
+{
+   if(n>10){
+    return;
+   }
+   console.log(n);
+   n+=1;
+   num(n);
+}
+num(1);*/
+function guess(n) {
+    let userGuess = Number(prompt("Enter a number:"));
+
+    if (userGuess == n) {
+        console.log("Congratulations! You guessed the number.");
+    } else if (userGuess < n) {
+        console.log("Too low! Try again.");
+        guess(n);
+    } else {
+        console.log("Too high! Try again.");
+        guess(n);
     }
 }
-else{
-    console.log("invalid username or password");
-}
-function cook()
-{
-    console.log("pour thr flour in tawa");
-    console.log("Rub it round order with gareta");
-    console.log("sprili some oil above it then wait");
-    console.log("twist it in dosa garenta");
-    console.log("atleat serve to padmanadhan");
-}
-for (let i=0;i<5;i++){
-    cook();
-}*/
-function dishwash(vessels){
-    console.log("we have to wash "+vessels+" vessels");
-}
+
+let number = Math.floor(Math.random() * 100) + 1;
+guess(number);
