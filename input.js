06acb2d58633c0446fd9d518e0c2554b1baa3184
srinivasanm095-boop <srinivasanm095@gmail.com>
2026-06-12 14:43:22 +0530

    HTML-CSS

---
+++ input.js
@@ -0,0 +1,17 @@
+function greet_afternoon() {
+    document.getElementById("greet").innerHTML = "Good Afternoon";
+}
+
+function turn_on() {
+    document.getElementById("on").src = "HTML-CSS/gettyimages-1279311730-640x640.jpg";
+}
+
+function turn_off() {
+    document.getElementById("on").src = "HTML-CSS/images.jpeg";
+}
+
+function change_color() {
+    document.getElementById("cap").style.backgroundColor = "black";
+    document.getElementById("Data").style.color = "white";
+    document.getElementById("greet").style.color = "white";
+}
