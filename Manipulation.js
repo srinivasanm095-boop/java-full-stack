086a3bc29c68a63ae58086aa958dacf375370db7
srinivasanm095-boop <srinivasanm095@gmail.com>
2026-06-12 11:59:47 +0530

    HTML-CSS

---
+++ Manipulation.js
@@ -0,0 +1,10 @@
+let value = document.getElementById("wish");
+console.log(pen);
+let mouse = document.getElementsByClassName("wishes");
+console.log(mouse);
+let cable = document.getElementsByTagName("div");
+console.log(cable);
+let water = document.querySelector("#content");
+console.log(water);
+let queries = document.getElementsByClassName("p");
+console.log(queries);
